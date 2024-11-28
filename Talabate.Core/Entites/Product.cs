c4e0d54e2c9f4e40287919c1f9de63fcdb02b307
MO Delewar <delewarmohamed@gmail.com>
2024-11-28 14:55:40 +0200

    Add project files.

---
+++ Talabate.Core/Entites/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabate.Core.Entites
+{
+    internal class Product
+    {
+        
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Describtion { get; set; }
+        public string PictureUrl { get; set; }
+        public decimal Price { get; set; }
+    }
+}
