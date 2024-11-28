ec2c9086f419dbb69aa5d435176e868148700bd2
MO Delewar <delewarmohamed@gmail.com>
2024-11-28 15:15:05 +0200

    Product Module

--- Talabate.Core/Entites/Product.cs
+++ Talabate.Core/Entites/Product.cs
@@ -6,13 +6,16 @@ using System.Threading.Tasks;
 
 namespace Talabate.Core.Entites
 {
-    internal class Product
+    internal class Product : BaseEntity
     {
-        
-        public int id { get; set; }
+
         public string Name { get; set; }
         public string Describtion { get; set; }
         public string PictureUrl { get; set; }
         public decimal Price { get; set; }
+        public int ProductBrandId { get; set; } //Fk
+        public ProductBrand ProductBrand { get; set; }
+        public int ProductTypeId { get; set; } //Fk 
+        public ProductType ProductType { get; set; }
     }
 }
---
+++ Talabate.Core/Entites/ProductType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabate.Core.Entites
+{
+    internal class ProductType:BaseEntity
+    {
+         
+        public string Name { get; set; }
+    }
+}
