805d4990a541305b44879fbabb4b496432f9f162
MO Delewar <delewarmohamed@gmail.com>
2024-11-28 16:43:18 +0200

    Product Configurations

--- Talabat.Repository/Data/StoreContext.cs
+++ Talabat.Repository/Data/StoreContext.cs
@@ -2,8 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Talabat.Repository.Data.Configurations;
 using Talabate.Core.Entites;
 
 namespace Talabat.Repository.Data
@@ -13,6 +15,16 @@ namespace Talabat.Repository.Data
         public StoreContext(DbContextOptions<StoreContext> options):base(options) 
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfiguration(new ProductConfigurations());
+            //modelBuilder.ApplyConfiguration(new ProductBrandConfigurations());
+            //modelBuilder.ApplyConfiguration(new ProductTypeConfigurations());
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
         }
 
         public DbSet<Product> Products { get; set; }
