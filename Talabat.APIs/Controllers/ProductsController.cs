2fdb9f10ce348541af8ec43e6ac3aa88112b6c79
MO Delewar <delewarmohamed@gmail.com>
2024-12-04 19:03:45 +0200

    Product Controller

---
+++ Talabat.APIs/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Talabate.Core.Entites;
+using Talabate.Core.Repositories;
+
+namespace Talabat.APIs.Controllers
+{
+    public class ProductsController : APIBaseController
+    {
+        private readonly IGenaricRepository<Product> _productRepo;
+
+        public ProductsController(IGenaricRepository<Product> productRepo)
+        {
+            _productRepo = productRepo;
+        }
+    }
+}
